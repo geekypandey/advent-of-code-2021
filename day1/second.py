245eacf1e6b61b3aa8bf180773be1e2c2e93961f
Anurag Pandey <anuragcarbon1998@gmail.com>
2021-12-03 09:00:51 +0530

    day1: part2 code clean up

--- day1/second.py
+++ day1/second.py
@@ -2,16 +2,9 @@ import sys
 
 with open(sys.argv[1]) as f:
     data = f.readlines()
+    data = [int(d) for d in data]
 
-data = [int(d) for d in data]
-
-count = 0
-for f, s, t, four in zip(data, data[1:], data[2:], data[3:]):
-    first = f+s+t
-    second = s+t+four
-    if second - first > 0:
-        count += 1
-
+count = sum(True for f, s in zip(data, data[3:]) if s - f > 0)
 
 print(count)
 
